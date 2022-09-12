24bf9944efb649a897458a7b1c95951e58d2c0f2
Beppino Defner <hallo@deona.at>
2022-09-12 20:21:22 +0200

    Generate tags and layout
    all tags needed for the basic tasks: div with backgroundColor and button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import './App.css';
-
 function App() {
-  return <div>Hello Planet Earth!</div>;
+  return (
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div
+        style={{
+          padding: '50px',
+          backgroundColor: '#ff6bb5',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+        }}
+      >
+        Generated Color:
+      </div>
+      <button>Generate</button>
+    </div>
+  );
 }
 
 export default App;
